6200868f67b09c857bb174d0fe42c4410e1492cc
unknown <devsouza98@outlook.com>
2021-01-08 10:15:20 -0300

    Add Footer

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -1,12 +1,14 @@
 import './App.css';
 import Home from './Home';
 import Navbar from './Navbar';
+import Footer from './Footer';
 
 function App(){
   return(
     <div>
       <Navbar />
       <Home />
+      <Footer />
     </div>
   );
 }
